07bb35c8c0d967f006d54f55ee960d688ec8cb36
Marina <31522051@087912-P33S105.MackSP.br>
2019-04-23 08:29:02 -0300

    Correção da classe de teste PessoaTest

--- sistema-de-chamados/test/entidade/PessoaTest.java
+++ sistema-de-chamados/test/entidade/PessoaTest.java
@@ -21,74 +21,41 @@ public class PessoaTest {
     public PessoaTest() {
     }
     
-    @BeforeClass
-    public static void setUpClass() {
+    //@BeforeClass
+    //public static void setUpClass() {
+    //}
+    
+    //@AfterClass
+    //public static void tearDownClass() {
+    //}
+    
+    //@Before
+    //public void setUp() {
+    //}
+    
+    //q@After
+    //public void tearDown() {
+    //}
+       
+    
+   
+    
+    @Test
+    public void criarUmaPessoaTest(){
+        Pessoa p = new Pessoa("Meu Nome", 123456789);
+        assertEquals("Meu Nome", p.getNome());
+        assertEquals(123456789, p.getTelefone());
+    } 
+    
+    @Test
+    public void criarUmaPessoaComTelefoneNwgativo() {
+        Pessoa p = new Pessoa ("Meu Nome", -123456789);
+        assertTrue(p.getTelefone() > 0);
     }
     
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
-
-    /**
-     * Test of getNome method, of class Pessoa.
-     */
-    @Test
-    public void testGetNome() {
-        System.out.println("getNome");
-        Pessoa instance = null;
-        String expResult = "";
-        String result = instance.getNome();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setNome method, of class Pessoa.
-     */
-    @Test
-    public void testSetNome() {
-        System.out.println("setNome");
-        String nome = "";
-        Pessoa instance = null;
-        instance.setNome(nome);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getTelefone method, of class Pessoa.
-     */
-    @Test
-    public void testGetTelefone() {
-        System.out.println("getTelefone");
-        Pessoa instance = null;
-        long expResult = 0L;
-        long result = instance.getTelefone();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setTelefone method, of class Pessoa.
-     */
-    @Test
-    public void testSetTelefone() {
-        System.out.println("setTelefone");
-        long telefone = 0L;
-        Pessoa instance = null;
-        instance.setTelefone(telefone);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    @Test(expected = Exception.class)
+    public void criarUmaPessoaComTelefoneNegativoComExcecaoTest(){
+        Pessoa p = new Pessoa("Meu Nome", -123456789);
     }
     
 }
